833cd653dd9d1b98a61de813092d8837dd9a9bcc
Lizbeth <lizbeth_96_libra@hotmail.com>
2023-01-05 14:23:30 -0500

    añadiendo funcion final

--- CalculadoraAngular/src/app/calculator/calculator.component.ts
+++ CalculadoraAngular/src/app/calculator/calculator.component.ts
@@ -13,30 +13,38 @@ export class CalculatorComponent {
   private resultado: number = 0;
   private operaciones: string[] = ['+', '-', '*', '/'];
   salida: string = '';
+  public seIncluyoUnaOperacionEnLaSalida: boolean = false;
 
   agregar(valor: string): void {
     if (this.operaciones.includes(valor)) {
       this.operador = valor;
       this.numeroActual = Number(this.salida.substring(1));
       this.salida = this.salida + valor;
-    } 
+    }
     else if (valor === '=') {
-      console.log(this.salida.split(this.operador)[1].substring(1));
-      this.numeroAnterior = Number(this.salida.split(this.operador)[1].substring(1));
-      this.calcular();
-    } 
+      if (this.seIncluyoUnaOperacionEnLaSalida) {
+        // cuando es =
+        console.log(this.salida.split(this.operador)[1].substring(1));
+        this.numeroAnterior = Number(this.salida.split(this.operador)[1].substring(1));
+        this.calcular();
+      } else {
+        // cuando es check
+        console.log(this.salida);
+      }
+    }
     else if (valor === 'C') {
       this.limpiar();
     }
     else if (this.salida === '') {
       this.salida = "$" + valor;
     }
-    else if (this.salida[this.salida.length-1] === this.operador) {
+    else if (this.salida[this.salida.length - 1] === this.operador) {
       this.salida = this.salida + "$" + valor;
     }
     else {
       this.salida = this.salida + valor;
     }
+    this.verificarSiLaSalidaTieneUnaOperacion();
   }
 
   calcular(): void {
@@ -56,15 +64,28 @@ export class CalculatorComponent {
     }
     this.salida = "$" + this.resultado.toString();
     console.log(this.numeroActual, this.numeroAnterior, this.operador, this.resultado);
+    this.verificarSiLaSalidaTieneUnaOperacion();
   }
 
   limpiar(): void {
     this.salida = '';
   }
-  retroceso():void{
-      this.salida = String(this.salida.slice(0,this.salida.length-1));
+
+  retroceso(): void {
+    this.salida = String(this.salida.slice(0, this.salida.length - 1));
+    this.verificarSiLaSalidaTieneUnaOperacion();
   }
 
+  verificarSiLaSalidaTieneUnaOperacion(): void {
+    if (this.salida.indexOf('+') != -1
+      || this.salida.indexOf('-') != -1
+      || this.salida.indexOf('*') != -1
+      || this.salida.indexOf('/') != -1) {
+      this.seIncluyoUnaOperacionEnLaSalida = true;
+    } else {
+      this.seIncluyoUnaOperacionEnLaSalida = false;
+    }
+  }
 }
 
 
